e3100531ac36596a7320ec644d5f0ee3bd2e7d65
Krystian Petek <krystianpetek2@gmail.com>
2023-03-15 07:34:03 +0100

    Added challenge solution with ScrollController

--- book-flutter_apprentice/two_foodapp/lib/screens/explore_screen.dart
+++ book-flutter_apprentice/two_foodapp/lib/screens/explore_screen.dart
@@ -1,12 +1,50 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:two_foodapp/components/components.dart';
 import 'package:two_foodapp/api/mock_foodapp_service.dart';
 import 'package:two_foodapp/models/models.dart';
 
-class ExploreScreen extends StatelessWidget {
-  ExploreScreen({super.key});
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({super.key});
 
+  @override
+  State<ExploreScreen> createState() => ExploreScreenState();
+}
+
+class ExploreScreenState extends State<ExploreScreen> {
   final mockService = MockFoodAppService();
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController = ScrollController();
+    _scrollController.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      printBottom();
+    }
+
+    if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      printTop();
+    }
+  }
+
+  void printTop() {
+    log('i am at the top!');
+  }
+
+  void printBottom() {
+    log('i am at the bottom!');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +53,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _scrollController,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),

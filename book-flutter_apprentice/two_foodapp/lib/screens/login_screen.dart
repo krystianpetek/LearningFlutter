52cab685648a3ffa8cadbb3a92daa0537d31167e
Krystian Petek <krystianpetek2@gmail.com>
2023-03-17 06:08:38 +0100

    Added LoginScreen with logo in assets

--- book-flutter_apprentice/two_foodapp/lib/screens/login_screen.dart
+++ book-flutter_apprentice/two_foodapp/lib/screens/login_screen.dart
@@ -1,11 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:two_foodapp/models/app_state_manager.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({super.key, this.username});
+
+  final String? username;
+
+  final Color logoColor = const Color.fromRGBO(64, 143, 77, 1);
+  final TextStyle focusedStyle = const TextStyle(color: Colors.green);
+  final TextStyle unfocusedStyle = const TextStyle(color: Colors.grey);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: ListView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            padding: const EdgeInsets.only(top: 44),
+            children: [
+              const SizedBox(
+                height: 200,
+                child: Image(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+              const SizedBox(height: 16),
+              buildTextField(username ?? 'username'),
+              const SizedBox(height: 16),
+              buildTextField('password'),
+              const SizedBox(height: 16),
+              buildButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(BuildContext context) {
+    return SizedBox(
+      height: 55,
+      child: MaterialButton(
+        color: logoColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () async {},
+      ),
+    );
+  }
+
+  Widget buildTextField(String hintText) {
+    return TextField(
+      cursorColor: logoColor,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.green,
+            width: 1.0,
+          ),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.green,
+          ),
+        ),
+        hintText: hintText,
+        hintStyle: const TextStyle(height: 0.5),
+      ),
+    );
   }
 }

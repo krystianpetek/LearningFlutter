153b3fdce84b7fbdf6dafd66e65c8df24d4c1764
Krystian Petek <krystianpetek2@gmail.com>
2023-03-17 18:44:29 +0100

    Fixed switch from card ToBuy to Recipes when no groceries yet in card

--- book-flutter_apprentice/two_foodapp/lib/screens/empty_grocery_screen.dart
+++ book-flutter_apprentice/two_foodapp/lib/screens/empty_grocery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:two_foodapp/models/models.dart';
 
@@ -29,17 +30,18 @@ class EmptyGroceryScreen extends StatelessWidget {
               textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.bodySmall,
             ),
-            const SizedBox(
-              height: 16,
-            ),
+            const SizedBox(height: 16),
             MaterialButton(
               textColor: Colors.white,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30)),
               color: Colors.green,
               onPressed: () {
-                Provider.of<AppStateManager>(context, listen: false)
-                    .goToRecipes();
+                // Provider.of<AppStateManager>(context, listen: false).goToRecipes();
+                context.goNamed(
+                  'home',
+                  params: {'tab': '${FoodAppTab.recipes}'},
+                );
               },
               child: const Text('Browse Recipes'),
             )
